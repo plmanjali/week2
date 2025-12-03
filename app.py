fb65223cfe8d8059a851f615733dce82e54c6863
plmanjali <anjalip3621@gmail.com>
2025-12-03 19:37:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("myform.html")
+@app.route('/submit', methods=['post'])
+def submit():
+    fname=request.form['fname']
+    lname=request.form['lname']
+    age=request.form['age']
+    return render_template('greetings.html',fname=fname, lname=lname, age=age)
+if(__name__=="__main__"):
+    app.run(debug=True)
